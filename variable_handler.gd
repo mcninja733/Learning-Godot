d2c927c992354485306ba4794c6bac7bb1f9dc05
cyanexis <mcninja733@gmail.com>
2026-07-11 01:25:26 -0400

    Create variable_handler.gd
    The bare base boneworks for my simple testing... This is gonna help me learn the structure inside of game development on exactly how variables are structured in comple things like charactes where a variable would need to be transferred a lot. This most likely would keep it nice and clean.

---
+++ variable_handler.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+
+#Main player stats
+
+#Perma stats
+var race := "N/A"
+var magic_affinity := 0
+
+#Changeable stats
+var move_speed := 3.0
+var acceleration := 20.0
+var rotation_speed := 12.0
+var jump_power := 12.0
+var player_gravity := -30.0
+
+
+
+
+
+
+#Behind the scenes
+
+#"Enabled" variables
+var camera_enabled := true
+var movement_enabled := true
