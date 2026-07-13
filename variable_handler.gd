95bffe853e8b2ce216eb1ee80a608faa7a3547a0
cyanexis <mcninja733@gmail.com>
2026-07-13 02:33:35 -0400

    Update variable_handler.gd
    fixed a bug that came along with more in depth percents. due to the  number being generated being only 1-100, it cant diffrentiate 0.1 and 0.5 so it will go to 0.1 since its better. so, to go more precise i multiplied the number being subtracted by 10

--- variable_handler.gd
+++ variable_handler.gd
@@ -1,34 +1,3 @@
-extends Node3D
-
-
-#Main player stats
-
-#Perma stats
-var race := "N/A"
-var magic_affinity := 0
-
-#Changeable stats
-var move_speed := 3.0
-var acceleration := 20.0
-var rotation_speed := 12.0
-var jump_power := 12.0
-var player_gravity := -30.0
-
-
-
-
-
-
-#Behind the scenes
-
-#"Enabled" variables
-var camera_enabled := true
-var movement_enabled := true
-
-
-
-# Race rolling function that is called upon from main menu for now
-
 var _race_rarities = {
 	Common = 60,
 	Rare = 35,
@@ -38,10 +7,10 @@ var _race_rarities = {
 }
 
 func race_gacha():
-	var _gachanumb = randi_range(1, 100)
+	var _gachanumb = randi_range(1, 1000)
 	print(_gachanumb)
 	for rarity in _race_rarities:
-		_gachanumb -= _race_rarities[rarity]
+		_gachanumb -= _race_rarities[rarity] * 10
 		
 		if _gachanumb <= 0:
 			return rarity
