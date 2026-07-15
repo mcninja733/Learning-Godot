366fc1c4ae1b862689350ed8b97fb9264f6ee4f4
cyanexis <mcninja733@gmail.com>
2026-07-14 23:49:29 -0400

    Update variable_handler.gd

--- variable_handler.gd
+++ variable_handler.gd
@@ -1,3 +1,34 @@
+extends Node3D
+
+
+#Main player stats
+
+#Perma stats
+var race := "N/A"
+var magic_affinity := 0
+
+#Changeable stats
+var move_speed := 3.0
+var acceleration := 20.0
+var rotation_speed := 12.0
+var jump_power := 12.0
+var player_gravity := -30.0
+
+
+
+
+
+
+#Behind the scenes
+
+#"Enabled" variables
+var camera_enabled := true
+var movement_enabled := true
+
+
+
+# Race rolling function that is called upon from main menu for now
+
 var _race_rarities = {
 	Common = 60,
 	Rare = 35,
