38c75018f39b4bd5a3fb9a076b9b3c1fad1199e4
cyanexis <mcninja733@gmail.com>
2026-07-15 23:30:26 -0400

    Update variable_handler.gd

--- variable_handler.gd
+++ variable_handler.gd
@@ -1,13 +1,13 @@
 extends Node3D
 
 
-#Main player stats
+# Main player stats
 
-#Perma stats
+# Perma stats
 var race := "N/A"
 var magic_affinity := 0
 
-#Changeable stats
+# Changeable stats
 var move_speed := 3.0
 var acceleration := 20.0
 var rotation_speed := 12.0
@@ -19,7 +19,7 @@ var player_gravity := -30.0
 
 
 
-#Behind the scenes
+# Behind the scenes
 
 #"Enabled" variables
 var camera_enabled := true
