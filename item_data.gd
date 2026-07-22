d2d97cd49b644049847b30a7389f5cd3f9dc8664
cyanexis <mcninja733@gmail.com>
2026-07-22 03:27:13 -0400

    Create item_data.gd
    Handles the general item data regarding inventory systems

---
+++ item_data.gd
@@ -0,0 +1,6 @@
+# Extending resource is the typical and standard thing for a much wider 
+# scale and should be used a ton for handling scripts. "res://" is short for it
+extends Resource
+
+@export var name : String = ""
+@export_multiline() var description : String = ""
