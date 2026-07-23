4fd5c308cda71a49dd25b364b93300becf4d18b4
cyanexis <mcninja733@gmail.com>
2026-07-23 03:18:06 -0400

    Update item_data.gd

--- item_data.gd
+++ item_data.gd
@@ -1,6 +1,9 @@
 # Extending resource is the typical and standard thing for a much wider 
 # scale and should be used a ton for handling scripts. "res://" is short for it
 extends Resource
+class_name ItemData
 
-@export var name : String = ""
-@export_multiline() var description : String = ""
+@export var name: String = ""
+@export_multiline() var description: String = ""
+@export var stackable: bool = false
+@export var texture: AtlasTexture
